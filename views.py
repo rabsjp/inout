b5d44ade22b457e5a6482dcb5f9f25dc4b27f339
Julio Hurtado <julioahurtado@gmail.com>
2018-12-28 15:31:55 -0800

    dynamic data being added to graph

--- views.py
+++ views.py
@@ -7,18 +7,6 @@ from .models import Constants
 class MyPage(Page):
     def vars_for_template(self):
         return {
-            "data": [
-                [0,1],
-                [1,2],
-                [2,3],
-                [3,4],
-                [4,5],
-                [5,6],
-                [6,7],
-                [7,8],
-                [8,9],
-                [9,10]
-            ],
             "gameConstant": 120, # Game constant to be determined by config file
         }
     
